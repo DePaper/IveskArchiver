77f5f08a8f16bfb57f4eb16fcd5b8a2da7aafa50
Šarūnas Jončas <s.joncas@dycode.net>
2024-12-04 14:57:35 +0200

    Less everything more comments

--- archiver.js
+++ archiver.js
@@ -1,40 +1,10 @@
 #!/usr/bin/env node
 
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 
-const API_URL = 'https://app.ivesk.lt/api/pub/digitized';
-
-function generatePath(outputpath, operation, myname, othername, date, docnum, id) {
-    return [
-        outputpath,
-        'Ivesk.lt',
-        myname,
-        date.substring(0, 7),
-        operation,
-        `${date} ${docnum.replace(/\s/g, '').replace(/[<>:"?/\\|*]/g, '_')} ${othername} ${id}.pdf`,
-    ];
-}
-
 /**
- * @param {object} document 
- * @returns {string[]}
- */
-function generateOutputStructure(document, outputpath) {
-    const result = [];
-
-    if (document.sellerisfavorite) {
-        result.push(generatePath(outputpath, 'Pardavimai', document.sellernameascii, document.buyernameascii, document.date, document.docnum, document.id));
-    }
-    if (document.buyerisfavorite) {
-        result.push(generatePath(outputpath, 'Pirkimai', document.buyernameascii, document.sellernameascii, document.date, document.docnum, document.id));
-    }
-
-    return result;
-}
-
-/**
- * 
+ * Download PDF file from Ivesk.lt and save it in outpuPath
  * @param {object} document 
  * @param {string} outputPath 
  * @returns {Promise<void>}
@@ -43,12 +13,22 @@ async function fetchFile(document, outputPath) {
     const resp = await fetch(document.url, { method: 'GET' });
     const file = Buffer.from(await resp.arrayBuffer());
 
-    const structure = generateOutputStructure(document, outputPath);
-    for (const item of structure) {
-        console.log(`Saving ${path.join(...item)}`);
-        const folders = path.join(...item.slice(0, item.length - 1));
-        await fs.mkdir(folders, { recursive: true });
-        await fs.writeFile(path.join(...item), file);
+    // clean up docnum so that would not break file name
+    document.docnum = document.docnum.replace(/[<>:"?/\\|*]/g, '_');
+
+    const filePaths = [];
+    if (document.sellerisfavorite) {
+        filePaths.push(path.join(outputPath, `${document.sellernameascii}/${document.date.substring(0, 7)}/Pardavimai/${document.date} ${document.docnum} ${document.buyernameascii}.pdf`));
+    }
+    if (document.buyerisfavorite) {
+        filePaths.push(path.join(outputPath, `${document.buyernameascii}/${document.date.substring(0, 7)}/Pirkimai/${document.date} ${document.docnum} ${document.sellernameascii}.pdf`));
+    }
+
+    for (const filePath of filePaths) {
+        console.log(`Saving ${filePath}`);
+        const folder = path.dirname(filePath);
+        fs.mkdirSync(folder, { recursive: true });
+        fs.writeFileSync(filePath, file);
     }
 
 }
@@ -57,52 +37,39 @@ async function fetchFile(document, outputPath) {
  * @param {string} filename 
  * @returns {object | null}
  */
-async function getLastPageFile(filename) {
+function getLastPageFile(filename) {
     try {
-        const result = await fs.readFile(filename, 'utf-8');
-        return JSON.parse(result);
+        return JSON.parse(fs.readFileSync(filename, 'utf-8'));
     } catch {
         return null;
     }
 }
 
-function printUsage() {
-    console.log('Usage: node archiver.js <apikey> <outputpath>');
-}
-
 async function main() {
     const args = process.argv.slice(2);
+    if (args.length < 2) {
+        console.error('Usage: node archiver.js <apikey> <outputpath>');
+        return 1;
+    }
     const apikey = args[0];
     const outputpath = args[1];
 
-    if (!apikey) {
-        console.error('"apiKey" is required.');
-        printUsage();
-        return;
+    // Load the "marker" where sinchronization finished last time. Will resume from it.
+    const lastPageFilePath = path.join(outputpath, `${apikey.substring(0, 36)}.json`);
+    const fromMarker = getLastPageFile(lastPageFilePath);
+
+    const url = new URL('https://app.ivesk.lt/api/pub/digitized');
+    url.searchParams.append('doctype', 'invoice');
+    if (fromMarker?.next) {
+        url.searchParams.append('from', fromMarker.next);
     }
-
-    if (!outputpath) {
-        console.error('"outputPath" is required.');
-        printUsage();
-        return;
-    }
-
-    const lastPageFileName = `${apikey.substring(0, 36)}.json`;
-    // Save the "next" page marker in the same output folder where the files will be saved.
-    const lastPageFilePath = path.join(outputpath, 'Ivesk.lt', lastPageFileName); 
-    const lastPageFile = await getLastPageFile(lastPageFilePath);
-
-    const options = {
-        method: 'GET',
-        headers: { 'x-api-key': apikey },
-    };
-
-    const url = new URL(API_URL);
-    if (lastPageFile?.next) { url.searchParams.append('from', lastPageFile.next); }
 
     let data = null;
     do {
-        const resp = await fetch(url, options);
+        const resp = await fetch(url, {
+            method: 'GET',
+            headers: { 'x-api-key': apikey },
+        });
         data = await resp.json();
 
         if (!resp.ok) {
@@ -110,13 +77,14 @@ async function main() {
         }
 
         // Not running this in parallel because there might be race conditions with mkdir. Running sequentially avoids that and it stil works pretty fast.
-        const invoices = data.documents .filter(doc => doc.doctype === 'invoice' && (doc.sellerisfavorite || doc.buyerisfavorite));
+        const invoices = data.documents.filter(doc => (doc.sellerisfavorite || doc.buyerisfavorite));
         for (const invoice of invoices) {
             await fetchFile(invoice, outputpath);
         }
 
         if (data.next) {
-            await fs.writeFile(lastPageFilePath, JSON.stringify({ next: data.next }));
+            // save the "marker" where sinchronization finished, will resume from here next time
+            fs.writeFileSync(lastPageFilePath, JSON.stringify({ next: data.next }));
         }
 
         url.searchParams.set('from', data.next);
